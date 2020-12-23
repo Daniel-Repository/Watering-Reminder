553619faedc4496f0faf94e9213b23067ed9fe8e
Daniel_C <danielcarrollbusiness@gmail.com>
2020-12-23 18:00:04 +0000

    Plant Class
    - Created initial Plant class. Holds Plant name, description, image, when it was last watered, and the period in which it should be watered next.

--- Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/MainActivity.java
+++ Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.plantsneedwater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtView1 = (TextView) findViewById(R.id.txtView1);
+        Button btnChangeText = (Button) findViewById(R.id.btnChangeText);
+        btnChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtView1.setText("Wow that worked!");
+            }
+        });
     }
 }
---
+++ Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/Plant.java
@@ -0,0 +1,46 @@
+package com.example.plantsneedwater;
+
+import android.media.Image;
+
+import java.time.Period;
+import java.util.Date;
+
+public class Plant {
+
+    private String plantName;
+    private String plantDescription;
+    private Image plantImage;
+    private Date plantLastWatered;
+    private Period plantNextWater;
+
+    public Plant(String pName, String pDesc, Image pImage, Date pLastW, Period pNextW) {
+        setPlantName(pName);
+        setPlantDescription(pDesc);
+        setPlantImage(pImage);
+        setPlantLastWatered(pLastW);
+        setPlantNextWater(pNextW);
+    }
+
+    //Getters
+    public String getPlantName() { return plantName; }
+
+    public String getPlantDescription() { return plantDescription; }
+
+    public Image getPlantImage() { return plantImage; }
+
+    public Date getPlantLastWatered() { return plantLastWatered; }
+
+    public Period getPlantNextWater() { return plantNextWater; }
+
+    //Setters
+    public void setPlantName(String plantName) { this.plantName = plantName; }
+
+    public void setPlantDescription(String plantDescription) { this.plantDescription = plantDescription; }
+
+    public void setPlantImage(Image plantImage) { this.plantImage = plantImage; }
+
+    public void setPlantLastWatered(Date plantLastWatered) { this.plantLastWatered = plantLastWatered; }
+
+    public void setPlantNextWater(Period plantNextWater) { this.plantNextWater = plantNextWater; }
+
+}
