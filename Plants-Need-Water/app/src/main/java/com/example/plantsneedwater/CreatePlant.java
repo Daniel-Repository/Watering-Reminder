51bceeba211893d2fa0467280753c359ac302490
Daniel_C <danielcarrollbusiness@gmail.com>
2020-12-30 00:44:02 +0000

    -

--- Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/CreatePlant.java
+++ Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/CreatePlant.java
@@ -3,6 +3,11 @@ package com.example.plantsneedwater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CreatePlant extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class CreatePlant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_plant);
+
+        TextInputEditText etPlantName = (TextInputEditText) findViewById(R.id.etPlantName);
+        EditText etPeriod = (EditText) findViewById(R.id.etPeriod);
+        Spinner spinPeriod = (Spinner) findViewById(R.id.spinPeriod);
+
+        //Populate spinPeriod with values from our string array 'period_array'
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.period_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinPeriod.setAdapter(adapter);
+
     }
 }
