800b452a0c44c8f3569778193b28922870907ae9
Daniel_C <danielcarrollbusiness@gmail.com>
2021-01-01 15:17:19 +0000

    'Last Watered' input & 'Plant Name' validation
    - Added input through a calendar date picker so the user can specify when the plant was last watered.
    - Validation for Plant Name field; if it's empty and you attempt to save, it'll throw an error.

--- Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/CreatePlant.java
+++ Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/CreatePlant.java
@@ -2,14 +2,24 @@ package com.example.plantsneedwater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Calendar;
+
 public class CreatePlant extends AppCompatActivity {
+
+    DatePickerDialog picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +29,46 @@ public class CreatePlant extends AppCompatActivity {
         TextInputEditText etPlantName = (TextInputEditText) findViewById(R.id.etPlantName);
         EditText etPeriod = (EditText) findViewById(R.id.etPeriod);
         Spinner spinPeriod = (Spinner) findViewById(R.id.spinPeriod);
+        ImageButton ibSave = (ImageButton) findViewById(R.id.ibSave);
 
         //Populate spinPeriod with values from our string array 'period_array'
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.period_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinPeriod.setAdapter(adapter);
+
+        EditText etEnterDate = (EditText) findViewById(R.id.etEnterDate);
+        etEnterDate.setInputType(InputType.TYPE_NULL);
+
+        //Open date picker dialog (Calendar) when select this edit text
+        etEnterDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(CreatePlant.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                etEnterDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        //When the SAVE button (in header) is selected
+        ibSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //VALIDATIONS
+                if(etPlantName.getText().toString().trim().length() == 0){
+                    etPlantName.setError("Please enter a name for your plant!");
+                }
+            }
+        });
 
     }
 }
