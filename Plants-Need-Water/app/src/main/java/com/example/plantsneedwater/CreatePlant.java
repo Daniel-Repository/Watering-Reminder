d0797ec3e529d024f8f72514713ced0993dcceb7
Daniel_C <danielcarrollbusiness@gmail.com>
2021-01-04 14:57:38 +0000

    Saving creates Plant object
    - Saving now creates a plant object.
    - Calcualtes the time between the date the user last watered the plant and the period the user sets that they should water it.
    - Pressing cancel button now re-opens Main Activity.

--- Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/CreatePlant.java
+++ Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/CreatePlant.java
@@ -3,6 +3,7 @@ package com.example.plantsneedwater;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -13,6 +14,7 @@ import android.content.Intent;
 
 import android.graphics.Bitmap;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.InputType;
@@ -28,34 +30,38 @@ import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreatePlant extends AppCompatActivity {
 
-    DatePickerDialog picker;
+    DatePickerDialog pickerDate;
+    ImageView ivPhoto;
+    Calendar cldr = Calendar.getInstance();
+    TextInputEditText etPlantName;
+    EditText etPeriod;
+    EditText etLastWateredDate;
+    Bitmap imageBitmap;
+    Spinner spinPeriod;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_plant);
 
-        TextInputEditText etPlantName = (TextInputEditText) findViewById(R.id.etPlantName);
-        EditText etPeriod = (EditText) findViewById(R.id.etPeriod);
-        ImageButton ibSave = (ImageButton) findViewById(R.id.ibSave);
+        etPlantName = findViewById(R.id.etPlantName);
+        etPeriod = findViewById(R.id.etPeriod);
+        etLastWateredDate = findViewById(R.id.etLastWateredDate);
 
         //USER SELECTS CAMERA AREA TO ADD NEW PHOTO
-        ImageView ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
-        LinearLayout llCameraField = (LinearLayout) findViewById(R.id.llCameraField);
-
-        ActivityResultLauncher<Intent> cameraResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-            if (result.getResultCode() == Activity.RESULT_OK) {
-                // There are no request codes
-                Intent data = result.getData();
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                ivPhoto.setImageBitmap(imageBitmap);
-            }
-        });
+        ivPhoto = findViewById(R.id.ivPhoto);
+        LinearLayout llCameraField = findViewById(R.id.llCameraField);
 
         llCameraField.setOnClickListener(v -> {
             Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -63,36 +69,105 @@ public class CreatePlant extends AppCompatActivity {
         });
 
         //Populate spinPeriod with values from our string array 'period_array'
-        Spinner spinPeriod = (Spinner) findViewById(R.id.spinPeriod);
+        spinPeriod = findViewById(R.id.spinPeriod);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.period_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinPeriod.setAdapter(adapter);
 
-        EditText etEnterDate = (EditText) findViewById(R.id.etEnterDate);
-        etEnterDate.setInputType(InputType.TYPE_NULL);
-
         //Open date picker dialog (Calendar) when select this edit text
-        etEnterDate.setOnClickListener(v -> {
-            final Calendar cldr = Calendar.getInstance();
+        etLastWateredDate.setOnClickListener(v -> {
             int day = cldr.get(Calendar.DAY_OF_MONTH);
             int month = cldr.get(Calendar.MONTH);
             int year = cldr.get(Calendar.YEAR);
-            // date picker dialog
-            picker = new DatePickerDialog(CreatePlant.this,
-                    (view, year1, monthOfYear, dayOfMonth) -> etEnterDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1), year, month, day);
-            picker.show();
-        });
+            //Date picker dialog
+            pickerDate = new DatePickerDialog(CreatePlant.this,
+                    (view, year1, monthOfYear, dayOfMonth) -> etLastWateredDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1), year, month, day);
+            pickerDate.getDatePicker().setMaxDate(System.currentTimeMillis());
+            pickerDate.show();
 
-        //When the SAVE button (in header) is selected
-        ibSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //VALIDATIONS
-                if(etPlantName.getText().toString().trim().length() == 0){
-                    etPlantName.setError("Please enter a name for your plant!");
+            pickerDate.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    cldr.set(year, month, dayOfMonth);
+                    etLastWateredDate.setText(dayOfMonth + "/" + (month +1) + "/" + year);
                 }
-            }
+            });
         });
 
+        //When the SAVE button (in header) is select
+        ImageButton ibSave = findViewById(R.id.ibSave);
+        ibSave.setOnClickListener(v -> {
+            savePlant();
+        });
+
+        //When the CANCEL button (in header) is selected
+        ImageButton ibCancel = findViewById(R.id.ibCancel);
+        ibCancel.setOnClickListener(v -> {
+            Intent intentMainAct = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intentMainAct);
+        });
     }
+
+    //CAMERA -> Launches and gets result (photo)
+    ActivityResultLauncher<Intent> cameraResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            ivPhoto.setImageBitmap(imageBitmap);
+        }
+    });
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void savePlant() {
+        boolean canSave = true;
+
+        //VALIDATIONS
+        if (etPlantName.getText().toString().trim().length() == 0) {
+            etPlantName.setError("Please enter a name for your plant!");
+            canSave = false;
+        } else {
+            canSave = true;
+        }
+
+        if (etPeriod.getText().toString().trim().length() == 0) {
+            etPeriod.setError("Please enter how often your plant should be watered!");
+            canSave = false;
+        } else {
+            canSave = true;
+        }
+
+        if (etLastWateredDate.getText().toString().trim().length() == 0) {
+            etLastWateredDate.setError("Please enter when your plant was last watered!");
+            canSave = false;
+        } else {
+            canSave = true;
+        }
+
+        if (canSave) {
+            String pName = etPlantName.getText().toString();
+            Date calendarDate = cldr.getTime();
+            LocalDate pLastWatered  = calendarDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            Plant newPlant = new Plant(pName, imageBitmap, pLastWatered, getPeriodIncrement());
+        }
+    }
+
+    private int getPeriodIncrement() {
+        int periodIncrement = 0;
+        int periodValue = Integer.parseInt(etPeriod.getText().toString());
+        String spinnerValue = spinPeriod.getSelectedItem().toString();
+        switch (spinnerValue) {
+            case "Day(s)":
+                periodIncrement = periodValue;
+                break;
+            case "Week(s)":
+                periodIncrement = periodValue * 7;
+                break;
+            case "Month(s)":
+                periodIncrement = periodValue * 30;
+                break;
+        }
+        return periodIncrement;
+    }
+
 }
--- Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/Plant.java
+++ Plants-Need-Water/app/src/main/java/com/example/plantsneedwater/Plant.java
@@ -2,46 +2,62 @@ package com.example.plantsneedwater;
 
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.os.Build;
+import android.util.Log;
 
+import androidx.annotation.RequiresApi;
+
+import java.time.LocalDate;
 import java.time.Period;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Plant {
 
+
     private String plantName;
     //private String plantDescription;
     private Bitmap plantImage;
-    private Date plantLastWatered;
+    private LocalDate plantLastWatered;
+    private int plantPeriodIncrement;
     private Period plantNextWater;
 
+    Calendar calendar = Calendar.getInstance();
+
     //Constructor
-    public Plant(String pName, Bitmap pImage, Date pLastW, Period pNextW) {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Plant(String pName, Bitmap pImage, LocalDate pLastW, int pPeriodIncrement) {
         setPlantName(pName);
-        //setPlantDescription(pDesc);
         setPlantImage(pImage);
         setPlantLastWatered(pLastW);
-        setPlantNextWater(pNextW);
+        setPlantPeriodIncrement(pPeriodIncrement);
+
+        setPlantNextWater(Period.between(pLastW, pLastW.plusDays(pPeriodIncrement)));
+        Log.d("Plant Details", pName + " " + pLastW.toString() + "  " + plantNextWater.toString());
     }
+
 
     //Getters
     public String getPlantName() { return plantName; }
 
-    //public String getPlantDescription() { return plantDescription; }
-
     public Bitmap getPlantImage() { return plantImage; }
 
-    public Date getPlantLastWatered() { return plantLastWatered; }
+    public LocalDate getPlantLastWatered() { return plantLastWatered; }
+
+    public int getPlantPeriodIncrement() { return plantPeriodIncrement; }
 
     public Period getPlantNextWater() { return plantNextWater; }
+
+
 
     //Setters
     public void setPlantName(String plantName) { this.plantName = plantName; }
 
-    //public void setPlantDescription(String plantDescription) { this.plantDescription = plantDescription; }
-
     public void setPlantImage(Bitmap plantImage) { this.plantImage = plantImage; }
 
-    public void setPlantLastWatered(Date plantLastWatered) { this.plantLastWatered = plantLastWatered; }
+    public void setPlantLastWatered(LocalDate plantLastWatered) { this.plantLastWatered = plantLastWatered; }
+
+    public void setPlantPeriodIncrement(int plantPeriodIncrement) { this.plantPeriodIncrement = plantPeriodIncrement; }
 
     public void setPlantNextWater(Period plantNextWater) { this.plantNextWater = plantNextWater; }
 
